eced56726310e4392d26b98dde11b1965a460d68
hauhuynh1208 <hauhuynhvnn@gmail.com>
2024-04-11 16:50:15 +0700

    id columns should be uuid

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,11 +1,10 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity } from './base.entity';
 
 @Entity()
-export class Users {
-  @PrimaryGeneratedColumn()
-  id: number;
+export class Users extends BaseEntity {
   @Column({ nullable: false, unique: true })
   email: string;
-  @Column({ nullable: false })
+  @Column({ nullable: false, select: false })
   password: string;
 }
